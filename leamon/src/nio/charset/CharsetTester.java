79f577e030b4cf276c15273b134819040a273fc8
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-11-08 02:46:57 +0000

    + 添加JDBC连接测试与字符集测试

---
+++ leamon/src/nio/charset/CharsetTester.java
@@ -0,0 +1,26 @@
+package nio.charset;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
+/**
+ * NIO ×Ö·û¼¯²âÊÔ
+ */
+public class CharsetTester {
+
+	public static void main(String[] args) throws Exception {
+		ByteBuffer buffer = ByteBuffer.allocate(100);
+		buffer.clear();
+		buffer.put("×æ´ºÀ×".getBytes());
+		buffer.flip();
+
+		Charset charset = Charset.forName("gbk");
+		CharsetDecoder decoder = charset.newDecoder();
+		CharBuffer charbuffer = decoder.decode(buffer);
+
+		System.out.println(charbuffer.array());
+
+	}
+}
