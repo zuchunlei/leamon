d4266c54c153fd2e715ec76021866d1b645694a1
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-11-19 04:55:24 +0000

    * 添加一些NIO测试中应该注意的问题。

--- leamon/src/nio/selector/SelectorTester.java
+++ leamon/src/nio/selector/SelectorTester.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 /**
  * 测试Selector的注册通道的线程安全性。 该实现会导致无数的线程被创建。
- * 因为selector.select()操作会立即返回，当accept时间没有被消费之前。
+ * 因为selector.select()操作会立即返回，当accept事件没有被消费之前。
  */
 public class SelectorTester {
 
@@ -37,8 +37,9 @@ public class SelectorTester {
 
 			while (it.hasNext()) {
 				SelectionKey key = it.next();
-				new Thread(new Handler(key)).start();
+				// 必须手动删除该选择键，否则在下次选择中，该选择键依然存在。
 				it.remove();
+				new Thread(new Handler(key)).start();
 			}
 		}
 	}
@@ -56,11 +57,15 @@ public class SelectorTester {
 				if (key.isAcceptable()) {
 					ServerSocketChannel servChannel = (ServerSocketChannel) key
 							.channel();
+					// accept 消费了“接受就绪”事件
 					SocketChannel channel = servChannel.accept();
 					channel.configureBlocking(false);
+					// 该channel第一次注册，如果该Selector处于select状态，则注册不成功。
+					// 因为竞争Selector实例的中publicKeys，导致channel注册阻塞。
 					channel.register(selector, SelectionKey.OP_READ);
 				} else if (key.isReadable()) {
 					SocketChannel channel = (SocketChannel) key.channel();
+					// read消费了“读继续”事件
 					channel.read(ByteBuffer.allocate(1024));
 					channel.register(selector, SelectionKey.OP_WRITE);
 				}
