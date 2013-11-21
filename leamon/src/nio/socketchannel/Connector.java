1f08b39445999dd7b98e85d9bb1d543f8450fcb5
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-11-21 06:25:13 +0000

    * 梳理一下SocketChannel作为客户端连接远程主机的主要操作步骤。

--- leamon/src/nio/socketchannel/Connector.java
+++ leamon/src/nio/socketchannel/Connector.java
@@ -1,6 +1,7 @@
 package nio.socketchannel;
 
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -12,10 +13,22 @@ public class Connector {
 
 	public static void main(String[] args) throws Exception {
 		Selector selector = Selector.open();
+
+		// 调用无参数的open(),将返回的信道配置为非阻塞的
 		SocketChannel sockChannel = SocketChannel.open();
 		sockChannel.configureBlocking(false);
 
-		sockChannel.connect(new InetSocketAddress("192.168.40.6", 12345));
+		// 通过给定的host/port尝试去连接远程主机，
+		sockChannel.connect(new InetSocketAddress("baidu.com", 80));
+		// 如果该连接处于pending状态，重复去连接
+		while (sockChannel.isConnectionPending()) {
+			sockChannel.finishConnect();
+		}
+
+		// 返回的是SocketAdaptor
+		Socket socket = sockChannel.socket();
+		System.out.println(socket);
+
 		sockChannel.register(selector, SelectionKey.OP_CONNECT);
 	}
 }
