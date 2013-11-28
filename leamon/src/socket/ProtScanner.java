ba44f85435c468799d8520cfa76d0cc434cc06bf
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-11-28 12:32:28 +0000

    * 通过Socket对象来检查主机开启的tcp端口，即端口扫描器的简单实现。

---
+++ leamon/src/socket/ProtScanner.java
@@ -0,0 +1,36 @@
+package socket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * 查询本地TCP端口是否处于监听状态
+ */
+public class ProtScanner {
+
+	public static void main(String[] args) {
+		String host = "localhost";
+		Socket sock = null;
+		for (int port = 1; port < 65535; port++) {
+			try {
+				sock = new Socket();
+				sock.connect(new InetSocketAddress(host, port), 1000);
+				System.out.println("port :" + port + " is openning!");
+			} catch (UnknownHostException e) {
+				break;
+			} catch (IOException e) {
+				// ignore
+			} finally {
+				if (sock != null) {
+					try {
+						sock.close();
+					} catch (IOException e) {
+					}
+				}
+			}
+		}
+	}
+
+}
