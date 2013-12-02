8cdcc3760fc4a77baa79188316eb55244a7ee39d
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-12-02 03:05:48 +0000

    * 修改NioServer之前版本遗留下来的问题，当前版本的NioServer无法在多线程模式下运行。

--- leamon/src/network/NioServer.java
+++ leamon/src/network/NioServer.java
@@ -50,26 +50,35 @@ public class NioServer {
 			ServerSocketChannel servChannel = (ServerSocketChannel) key
 					.channel();
 			SocketChannel channel = servChannel.accept();
-			channel.configureBlocking(false);
-			channel.register(key.selector(), SelectionKey.OP_READ);
+			channel.configureBlocking(false);// 必须将Channel配置为非阻塞模式下，才可用注册到Selector中。
+			channel.register(key.selector(), SelectionKey.OP_READ);// accept()操作返回的SocketChannel对象，不许显式的通过register()操作注册该信道。
 		} else if (key.isReadable()) {
+			// 第一步取消该信道的兴趣读，否者在多线程环境下会造成该读就绪事件被多个线程消费的现象。
+			key.interestOps(key.interestOps() ^ SelectionKey.OP_READ);
+
 			SocketChannel channel = (SocketChannel) key.channel();
 			ByteBuffer buffer = ByteBuffer.allocate(1000);
 			int len = channel.read(buffer);
 			if (len == -1) {
 				channel.close();
 			} else {
-				channel.register(key.selector(), SelectionKey.OP_READ
-						| SelectionKey.OP_WRITE);
+				// channel.register(key.selector(), SelectionKey.OP_READ
+				// | SelectionKey.OP_WRITE);
+				// 如果信道Channel已经与选择器Selector具有了注册关系，则该Channel的兴趣事件改变将由SelectionKey对象处理。
+				key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);// 信道关注兴趣写
 			}
 		} else if (key.isWritable()) {
+			// 取消信道的兴趣写关注
+			key.interestOps(key.interestOps() ^ SelectionKey.OP_WRITE);
+
 			SocketChannel channel = (SocketChannel) key.channel();
-			Date date = new Date();
 			ByteBuffer buffer = ByteBuffer.allocate(1000);
-			buffer.put(date.toString().getBytes());
+			buffer.put(new Date().toString().getBytes());
 			buffer.flip();
 			channel.write(buffer);
-			channel.register(key.selector(), SelectionKey.OP_READ);
+			// channel.register(key.selector(), SelectionKey.OP_READ);
+			// 使用SelectionKey来改变Channel的兴趣关注的改变
+			key.interestOps(key.interestOps() | SelectionKey.OP_READ);// 信道关注兴趣读
 		}
 	}
 }
