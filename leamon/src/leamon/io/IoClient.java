db6287eecb20df1a041db3041a2b2165ba60e57d
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-12-18 05:11:56 +0000

    + 添加BasicIoService，该类内部采用链表方式来实现IoFilterChain，其基本功能与BaseIoService一致。 * 修改IoClient，使用BasicIoService替代BaseIoService来完成IO操作。

--- leamon/src/leamon/io/IoClient.java
+++ leamon/src/leamon/io/IoClient.java
@@ -3,7 +3,7 @@ package leamon.io;
 public class IoClient {
 
 	public static void main(String[] args) {
-		IoService service = new BaseIoService(new DefaultIoHandler());
+		IoService service = new BasicIoService(new DefaultIoHandler());
 		for (int i = 0; i < 12; i++) {
 			service.addFilter(new DefaultIoFilter());
 		}
