c9c00e3b858fe23d51fcf2239eeea335c3101eaa
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-12-22 09:57:34 +0000

    + 添加MemberShipServer，该组件供外界调用，用来获得同级服务节点的列表数组。包装节点发现功能的实现细节，在设计模式中属于门面（Facade）模式。

---
+++ leamon/src/socket/udp/MemberShipServer.java
@@ -0,0 +1,18 @@
+package socket.udp;
+
+import socket.udp.multicast.Member;
+
+/**
+ * UDP的Member发现功能模块
+ */
+public class MemberShipServer {
+
+	/**
+	 * 获得其它同级节点Member的列表数组
+	 * 
+	 * @return
+	 */
+	public Member[] getMembers() {
+		return null;
+	}
+}
