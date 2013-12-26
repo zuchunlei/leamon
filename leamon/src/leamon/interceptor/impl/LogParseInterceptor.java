af8c75156e34f47962e137fcfa1a9ca4ad552309
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-12-26 07:11:14 +0000

    + 根据不同需求创建不同类型的拦截器，其中Compress为压缩拦截器，LogParse为日志解析拦截器，Capacity为容量限制拦截器。

---
+++ leamon/src/leamon/interceptor/impl/LogParseInterceptor.java
@@ -0,0 +1,19 @@
+package leamon.interceptor.impl;
+
+import leamon.interceptor.InterceptorAdapter;
+import leamon.interceptor.MessagePacket;
+
+/**
+ * 日志解析
+ */
+public class LogParseInterceptor extends InterceptorAdapter {
+
+	/**
+	 * 对数据包进行日志解析处理
+	 * 
+	 * @param packet
+	 */
+	@Override
+	protected void doInvoke(MessagePacket packet) {
+	}
+}
