8416e681f6e6f3d84fa70b67b92a82b27e0fe909
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2013-12-26 06:31:20 +0000

    + 添加基于数据包处理的拦截器与处理器结构模型。其中MessagePacket为数据对象模型，内部封装待处理的数据。其中Interceptor，InterceptorChain，Handler均是拦截器链/处理器模式的接口。

---
+++ leamon/src/leamon/interceptor/Interceptor.java
@@ -0,0 +1,10 @@
+package leamon.interceptor;
+
+/**
+ * À¹½ØÆ÷½Ó¿Ú
+ */
+public interface Interceptor {
+
+	void invoke(final MessagePacket packet, InterceptorChain chian);
+	
+}
---
+++ leamon/src/leamon/interceptor/MessagePacket.java
@@ -0,0 +1,36 @@
+package leamon.interceptor;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 数据包对象
+ */
+public class MessagePacket implements Serializable {
+	private static final long serialVersionUID = 5378422410294437807L;
+
+	private Map<String, Object> header;
+	private byte[] body;
+
+	public MessagePacket() {
+		this(null);
+	}
+
+	public MessagePacket(byte[] body) {
+		this.body = body;
+		this.header = new HashMap<String, Object>();
+	}
+
+	public void addHeader(String key, Object value) {
+		header.put(key, value);
+	}
+
+	public byte[] getBody() {
+		return body;
+	}
+
+	public void setBody(byte[] body) {
+		this.body = body;
+	}
+}
