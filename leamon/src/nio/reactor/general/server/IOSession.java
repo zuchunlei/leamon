b81805eeb97155e17cdfeb2a812f37f937935f7c
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-01-15 12:09:45 +0000

    * 修改IOSession类的实现，内部持有数据缓冲区对象Buffer与Poller对象，在IO操作完成后修改对就绪事件的关注操作。 # 当前读取数据的格式，按照echo协议要求（对字节流数据保存不变），将在后续版本进行通用协议读写的实现。

--- leamon/src/nio/reactor/general/server/IOSession.java
+++ leamon/src/nio/reactor/general/server/IOSession.java
@@ -6,36 +6,63 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import nio.reactor.general.server.Server.Poller;
+
 /**
  * IOSession封装了一个网络连接的读写操作，并且内部维护了读写过程中需要的状态信息。
  */
 public class IOSession {
+	private static final int DEFAULT_SIZE = 1024;
 
 	private SelectionKey key;// key对象包含了io就绪事件
 	private volatile AtomicBoolean inReading;// 是否处于读状态
+	private ByteBuffer buffer;// 数据缓冲区
 
-	public IOSession(SelectionKey key) {
+	private Poller poller;// 轮询器
+
+	public IOSession(SelectionKey key, Poller poller) {
 		this.key = key;
+		this.poller = poller;
 		this.inReading = new AtomicBoolean();// 默认为false
+		this.buffer = ByteBuffer.allocate(DEFAULT_SIZE);
 	}
 
 	public boolean isReading() {
 		return inReading.get();
 	}
 
+	/**
+	 * 读取数据，OP_READ就绪事件的处理方法，内核态--->用户态
+	 */
 	public void readData() {
 		if (inReading.compareAndSet(false, true)) {
 			SocketChannel channel = (SocketChannel) key.channel();
 			try {
-				channel.read(ByteBuffer.allocate(1024));
+				channel.read(buffer);
 			} catch (IOException e) {
-				e.printStackTrace();
+				// 在channel读写时发生异常
+				this.key.cancel();
+				try {
+					channel.close();
+				} catch (IOException ioe) {
+					// ignore
+				}
+				return;
 			}
 			inReading.compareAndSet(true, false);
 		}
+		// 注册写就绪关注
+		this.poller.registerWrite(key);
 	}
 
+	/**
+	 * 写入数据，OP_WRITE就绪事件的处理方法，用户态--->内核态
+	 */
 	public void writeData() {
+		
+		
+		// 注册读就绪关注
+		this.poller.registerRead(key);
 	}
 
 	/**
