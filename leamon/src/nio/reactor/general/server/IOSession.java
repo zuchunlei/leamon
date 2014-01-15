8d26d85c19dc727bff5d72525fcf7e8eecb01527
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-01-15 12:41:07 +0000

    * 修改IOSession的内部实现，添加onReadComplete与onWriteReady方法，分别处理网络读完成操作以及网络写就绪操作。并且引用DataPacket对象进行网络与应用层间的数据传输。

--- leamon/src/nio/reactor/general/server/IOSession.java
+++ leamon/src/nio/reactor/general/server/IOSession.java
@@ -6,6 +6,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import nio.reactor.general.data.DataPacket;
 import nio.reactor.general.server.Server.Poller;
 
 /**
@@ -17,11 +18,14 @@ public class IOSession {
 	private SelectionKey key;// key对象包含了io就绪事件
 	private volatile AtomicBoolean inReading;// 是否处于读状态
 	private ByteBuffer buffer;// 数据缓冲区
-
 	private Poller poller;// 轮询器
+
+	private SocketChannel channel;// IO信道
+	private DataPacket packet;
 
 	public IOSession(SelectionKey key, Poller poller) {
 		this.key = key;
+		this.channel = (SocketChannel) key.channel();
 		this.poller = poller;
 		this.inReading = new AtomicBoolean();// 默认为false
 		this.buffer = ByteBuffer.allocate(DEFAULT_SIZE);
@@ -36,9 +40,16 @@ public class IOSession {
 	 */
 	public void readData() {
 		if (inReading.compareAndSet(false, true)) {
-			SocketChannel channel = (SocketChannel) key.channel();
+			// SocketChannel channel = (SocketChannel) key.channel();
 			try {
 				channel.read(buffer);
+
+				// 数据读取完成，构造DataPacket进行应用层处理
+				packet = new DataPacket(channel.getRemoteAddress(), buffer);
+
+				// 网络读完成（数据已从内核态到用户态），进行应用层处理
+				onReadComplete(packet);
+
 			} catch (IOException e) {
 				// 在channel读写时发生异常
 				this.key.cancel();
@@ -59,10 +70,43 @@ public class IOSession {
 	 * 写入数据，OP_WRITE就绪事件的处理方法，用户态--->内核态
 	 */
 	public void writeData() {
-		
-		
+		// 应用层数据写处理
+		onWriteReady(packet);
+
+		// 进行网络写操作
+		try {
+			channel.write(packet.getContent());
+		} catch (IOException e) {
+			// 在channel读写时发生异常
+			this.key.cancel();
+			try {
+				channel.close();
+			} catch (IOException ioe) {
+				// ignore
+			}
+			return;
+		}
+
 		// 注册读就绪关注
 		this.poller.registerRead(key);
+	}
+
+	/**
+	 * 网络读完成，进行应用层处理
+	 * 
+	 * @param packet
+	 */
+	public void onReadComplete(DataPacket packet) {
+
+	}
+
+	/**
+	 * 网络写就绪，进行应用层处理
+	 * 
+	 * @param packet
+	 */
+	public void onWriteReady(DataPacket packet) {
+
 	}
 
 	/**
