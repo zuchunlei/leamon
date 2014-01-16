fe06fc60fab7d656f86426db7bd9ad60ef8476da
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-01-16 02:26:39 +0000

    * 修改IOReadWrok与IOWriteWork类，该类型主要驱动IOSession进行IO操作，由Poller中的线程池执行器executor执行调用。

--- leamon/src/nio/reactor/general/server/IOReadWork.java
+++ leamon/src/nio/reactor/general/server/IOReadWork.java
@@ -1,40 +1,43 @@
 package nio.reactor.general.server;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.SocketChannel;
 
 /**
  * 具体IO读事件处理的任务对象，由外部线程池调用。
  */
 public class IOReadWork implements Runnable {
 
-	private Server.Poller poller;
-	private SelectionKey key;
+	// private Server.Poller poller;
+	// private SelectionKey key;
+	//
+	// private ByteBuffer buffer;
+	//
+	// public IOReadWork(Server.Poller poller, SelectionKey key) {
+	// this.poller = poller;
+	// this.key = key;
+	// this.buffer = ByteBuffer.allocate(1024);
+	// }
 
-	private ByteBuffer buffer;
+	private IOSession session;
 
-	public IOReadWork(Server.Poller poller, SelectionKey key) {
-		this.poller = poller;
-		this.key = key;
-		this.buffer = ByteBuffer.allocate(1024);
+	public IOReadWork(IOSession session) {
+		this.session = session;
 	}
 
 	public void run() {
 		// 读取数据，取消兴趣读，业务处理，注册兴趣写。
-		SocketChannel channel = (SocketChannel) key.channel();
-		try {
-			if (channel.read(buffer) == -1) {
-				key.cancel();
-				key.channel().close();
-				return;
-			}
-			// unRegisterRead();
-			// poller.putData(key, buffer);
-			key.attach(buffer);// 将缓冲区以SelectionKey附件的方式挂载
-			poller.registerWrite(key);
-		} catch (IOException e) {
-		}
+		// SocketChannel channel = (SocketChannel) key.channel();
+		// try {
+		// if (channel.read(buffer) == -1) {
+		// key.cancel();
+		// key.channel().close();
+		// return;
+		// }
+		// // unRegisterRead();
+		// // poller.putData(key, buffer);
+		// key.attach(buffer);// 将缓冲区以SelectionKey附件的方式挂载
+		// poller.registerWrite(key);
+		// } catch (IOException e) {
+		// }
+		session.readData();
 	}
 }
--- leamon/src/nio/reactor/general/server/IOWriteWork.java
+++ leamon/src/nio/reactor/general/server/IOWriteWork.java
@@ -1,36 +1,37 @@
 package nio.reactor.general.server;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.SocketChannel;
-
-import nio.reactor.general.server.Server.Poller;
 
 /**
  * 具体IO事件处理的任务对象，由外部线程池调用。
  */
 public class IOWriteWork implements Runnable {
-	private Server.Poller poller;
-	private SelectionKey key;
+	// private Server.Poller poller;
+	// private SelectionKey key;
+	//
+	// public IOWriteWork(Poller poller, SelectionKey key) {
+	// this.poller = poller;
+	// this.key = key;
+	// }
 
-	public IOWriteWork(Poller poller, SelectionKey key) {
-		this.poller = poller;
-		this.key = key;
+	private IOSession session;
+
+	public IOWriteWork(IOSession session) {
+		this.session = session;
 	}
 
 	// 取消兴趣写，写数据到信道，注册兴趣读。
 	public void run() {
-		SocketChannel channel = (SocketChannel) key.channel();
-		try {
-			// unRegisterWrite();
-			// ByteBuffer buffer = poller.getData(key);
-			ByteBuffer buffer = (ByteBuffer) key.attachment();// 以附件的方式获取读写缓冲区
-			buffer.flip();
-			channel.write(buffer);
-			poller.registerRead(key);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// SocketChannel channel = (SocketChannel) key.channel();
+		// try {
+		// // unRegisterWrite();
+		// // ByteBuffer buffer = poller.getData(key);
+		// ByteBuffer buffer = (ByteBuffer) key.attachment();// 以附件的方式获取读写缓冲区
+		// buffer.flip();
+		// channel.write(buffer);
+		// poller.registerRead(key);
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		session.writeData();
 	}
 }
