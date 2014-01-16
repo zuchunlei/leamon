beab0aaef140ffdfc3408d03e2a1b464df9c9739
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-01-16 02:50:35 +0000

    + 添加应用层数据处理包，对数据的应用层处理采用拦截器链filterchain与处理器handler模式。对数据流进行层次化过滤与业务逻辑处理。

---
+++ leamon/src/nio/reactor/general/io/IOFilterChain.java
@@ -0,0 +1,25 @@
+package nio.reactor.general.io;
+
+import nio.reactor.general.data.DataPacket;
+
+/**
+ * IO数据过滤器
+ */
+public interface IOFilterChain {
+
+	/**
+	 * 网络数据读完成，内核态----->用户态 （完成）
+	 * 
+	 * @param packet
+	 */
+	void onReadComplete(DataPacket packet);
+
+	/**
+	 * 应用数据写就绪，用户态----->内核态 （就绪）
+	 * 
+	 * @param packet
+	 * @return
+	 */
+	DataPacket onWriteReady(DataPacket packet);
+
+}
