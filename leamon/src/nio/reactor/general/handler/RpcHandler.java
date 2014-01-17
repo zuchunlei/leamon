879d3ebcc78d7eb0ba17fcdb0e95704c1f298a8b
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-01-17 06:09:14 +0000

    + 添加handler包，该包下为常用协议处理器的默认实现。并提供EchoHandler与RpcHandler的默认实现。

---
+++ leamon/src/nio/reactor/general/handler/RpcHandler.java
@@ -0,0 +1,16 @@
+package nio.reactor.general.handler;
+
+import nio.reactor.general.data.DataPacket;
+import nio.reactor.general.io.IOHandler;
+
+/**
+ * RPC Handler
+ */
+public class RpcHandler implements IOHandler {
+
+	@Override
+	public DataPacket handle(DataPacket packet) {
+		return null;
+	}
+
+}
