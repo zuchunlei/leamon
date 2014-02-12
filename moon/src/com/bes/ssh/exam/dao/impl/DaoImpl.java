573c1548a09e720b2f8697e8b5415ccdf186ccd6
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-02-12 09:22:51 +0000

    * 在Dao类中实现数据库操作方法，通过DataSource对象获得Connection对象进行对数据库的操作。

--- moon/src/com/bes/ssh/exam/dao/impl/DaoImpl.java
+++ moon/src/com/bes/ssh/exam/dao/impl/DaoImpl.java
@@ -1,5 +1,9 @@
 package com.bes.ssh.exam.dao.impl;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.sql.DataSource;
 
 import com.bes.ssh.exam.dao.Dao;
@@ -15,8 +19,19 @@ public class DaoImpl implements Dao {
 		this.dataSource = dataSource;
 	}
 
-	public void add(String name, String password) {
+	public void add(String name, String password)  {
 		// operate db via the dataSource object
-		System.out.println(dataSource);
+		String sql = "insert into person(name,password) values ('" + name
+				+ "','" + password + "')";
+
+		try {
+			Connection conn = dataSource.getConnection();
+			Statement stmt = conn.createStatement();
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
+
 }
