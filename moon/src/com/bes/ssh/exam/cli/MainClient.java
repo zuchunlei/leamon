bd1302146b05700df6e33a788452ca6b879cb39f
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-02-12 09:28:25 +0000

    + 添加测试类MainClient，对exam模块进行事务相关时测试。 # 当前操作中，业务对象service.add方法在执行过程中抛出运行时异常，此时应该事务回滚，而当前配置状态下，事务没有进行回滚。

---
+++ moon/src/com/bes/ssh/exam/cli/MainClient.java
@@ -0,0 +1,22 @@
+package com.bes.ssh.exam.cli;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bes.ssh.exam.service.Service;
+
+/**
+ * 测试类
+ */
+public class MainClient {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"application-context-exam.xml");
+
+		// 获得业务对象，该对象为Spring容器装配过的代理对象，内部织入了应用服务基础设施，如：事务等。
+		Service service = (Service) context.getBean("service");
+
+		// 调用业务方法，如果业务方法执行中抛出运行时异常，则事务回滚。
+		service.add("zcl", "zcl");
+	}
+}
