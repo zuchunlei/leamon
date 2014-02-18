b6673c5c5b3c9f10f6414bb58328c97cdb5af1b6
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-02-18 10:09:36 +0000

    * 修改Dao的实现，使其继承JdbcDaoSupport。不再主动持有dataSource对象，数据库操作使用内部的jdbcTemplate对象来执行。 # 注入dataSource对象到dao中，由JdbcDaoSupport用来构建jdbcTemplate对象。

--- moon/src/com/bes/ssh/exam/dao/impl/DaoImpl.java
+++ moon/src/com/bes/ssh/exam/dao/impl/DaoImpl.java
@@ -1,37 +1,18 @@
 package com.bes.ssh.exam.dao.impl;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.bes.ssh.exam.dao.Dao;
 
 /**
  * POJO形式的数据访问对象实现
  */
-public class DaoImpl implements Dao {
+public class DaoImpl extends JdbcDaoSupport implements Dao {
 
-	private DataSource dataSource;
-
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
-	public void add(String name, String password)  {
+	public void add(String name, String password) {
 		// operate db via the dataSource object
 		String sql = "insert into person(name,password) values ('" + name
 				+ "','" + password + "')";
-
-		try {
-			Connection conn = dataSource.getConnection();
-			Statement stmt = conn.createStatement();
-			stmt.execute(sql);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		getJdbcTemplate().execute(sql);
 	}
-
 }
