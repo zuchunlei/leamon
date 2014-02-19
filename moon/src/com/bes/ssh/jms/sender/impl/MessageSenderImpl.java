03b93efd68663beab8cff2c91f03ee9d4e4b9109
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-02-19 07:07:28 +0000

    * 修改MessageSenderImpl，继承JmsGatewaySupport，自身不再维护JmsTemplate对象。在Spring中对自身的配置不改变。

--- moon/src/com/bes/ssh/jms/sender/impl/MessageSenderImpl.java
+++ moon/src/com/bes/ssh/jms/sender/impl/MessageSenderImpl.java
@@ -2,23 +2,17 @@ package com.bes.ssh.jms.sender.impl;
 
 import java.util.Map;
 
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.support.JmsGatewaySupport;
 
 import com.bes.ssh.jms.sender.MessageSender;
 
 /**
  * JMS 消息发送者
  */
-public class MessageSenderImpl implements MessageSender {
-
-	// JMS模板
-	private JmsTemplate jmsTemplate;
-
-	public void setJmsTemplate(JmsTemplate jmsTemplate) {
-		this.jmsTemplate = jmsTemplate;
-	}
+public class MessageSenderImpl extends JmsGatewaySupport implements
+		MessageSender {
 
 	public void sendMessage(final Map<String, Object> message) {
-		jmsTemplate.convertAndSend(message);
+		getJmsTemplate().convertAndSend(message);
 	}
 }
