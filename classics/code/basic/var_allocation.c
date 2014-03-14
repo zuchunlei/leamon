b01bcb54a210b1d50af2bda097e8b2a3dc0bccf5
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-03-14 09:52:38 +0000

    * 在函数中添加对静态局部变量的储存分配测试。静态局部变量在静态储存时，变量的标识符将被改写为变量名.随机数的方式。 # 为初始化的静态局部变量，存储在.bss段。已初始化的静态局部变量，存储在.data段中。

--- classics/code/basic/var_allocation.c
+++ classics/code/basic/var_allocation.c
@@ -3,7 +3,7 @@
 // 变量分配，储存相关
 
 // 全局变量，未初始化全局变量，静态分配。存储在.bss数据段中。
-int num;
+int number;
 short age;
 
 // 以初始化全局变量，静态分配。存储在.data数据段中。
@@ -15,6 +15,11 @@ int main(){
 	
 	// 函数的局部变量/参数，动态分配。存储在栈内存中。
 	int num = 20;
+
+	static int si;// 未初始化的静态局部变量，分配在.bss段
+	static int sii = 100;//已初始化的静态局部变量，分配在.data段
+
+
 	printf("function main var num is %d\n",num);
 
 }
