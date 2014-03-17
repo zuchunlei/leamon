6cb1dc38972b194e426487ee61531aa4f35d20c5
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-03-17 03:50:26 +0000

    + 添加结构体内存对齐测试文件，并对结构体内存对齐原则进行研究。Linux下结构体内存对齐的默认长度为4字节，即#pragma pack(4) # 以下为结构体内存对齐原则： # 1、对于结构的各个成员，第一个成员位于偏移为0的位置，以后每个数据成员的偏移量必须是min(#pragma pack()指定的数，这个数据成员的自身长度) 的倍数。 # 2、在数据成员完成各自对齐之后，结构(或联合)本身也要进行对齐，对齐将按照#pragma pack指定的数值和结构(或联合)最大数据成员长度中，比较小的那个进行。

---
+++ classics/code/basic/memory_alignment.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// 结构体的内存对齐测试
+
+struct Person
+{
+	char sex; // 1个字节
+	int number; // 4字节
+	short age; // 2字节
+	char *name; // 4字节
+	double price; // 8字节
+	
+}; // 以4字节（默认）对齐，该类型结构体变量占用24字节
+
+
+struct Student
+{
+	char sex;
+	short age;
+	int number;
+	char *name;
+	double price;
+}; // 20字节
+
+
+int main(){
+
+	// 定义了一个结构体类型的变量
+	struct Person p ; // 分配内存
+	p.sex = 'w';
+	p.number = 1;
+	p.age = 28;
+	p.name = "zuchunlei";
+	p.price = 20.00;
+
+	struct Student s;
+	s.sex = 'm';
+	s.number = 2;
+	s.age = 27;
+	s.name = "lidaqi";
+	s.price = 200.00;
+	
+	printf("struct type Person is sizeof :%d\n",sizeof(p));
+	printf("struct type Student is sizeof :%d\n",sizeof(s));
+
+}
+
+// 以下为结构体内存对齐原则：
+// 1、对于结构的各个成员，第一个成员位于偏移为0的位置，以后每个数据成员的偏移量必须是min(#pragma pack()指定的数，这个数据成员的自身长度) 的倍数。
+// 2、在数据成员完成各自对齐之后，结构(或联合)本身也要进行对齐，对齐将按照#pragma pack指定的数值和结构(或联合)最大数据成员长度中，比较小的那个进行。
