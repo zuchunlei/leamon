1e4c045960b7be03c9b42f0f025dcb6a47de9e53
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-06-06 11:38:58 +0000

    + 添加类型转换器的测试类，该测试以多格式的日期字符串为输入，验证不同格式的输入是否可以正确转换。

---
+++ leamon/src/leamon/converter/MainClient.java
@@ -0,0 +1,32 @@
+package leamon.converter;
+
+import java.util.Date;
+
+import leamon.converter.impl.DateConverter;
+
+/**
+ * 类型转换器测试主类
+ */
+public class MainClient {
+
+	public static void main(String[] args) {
+		TypeConverters
+				.addConverter(Date.class, new DateConverter("yyyy-MM-dd"));
+		TypeConverters
+				.addConverter(Date.class, new DateConverter("yyyy/MM/dd"));
+		TypeConverters
+				.addConverter(Date.class, new DateConverter("yyyy~MM~dd"));
+
+		Date result;
+
+		result = TypeConverters.converte(Date.class, "1986-10-24");
+		System.out.println(result);
+
+		result = TypeConverters.converte(Date.class, "1997/9/1");
+		System.out.println(result);
+
+		result = TypeConverters.converte(Date.class, "2008~9~10");
+		System.out.println(result);
+	}
+
+}
