5d16c5041ea91dbad22aa4aee116870964033e2e
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-06-25 12:01:37 +0000

    * 将Cow类型封装成为树节点，使用左孩子右兄弟的方式存储。对Cow类型提供获得孩子与获得兄弟的方法。

--- leamon/src/leamon/practice/Farm.java
+++ leamon/src/leamon/practice/Farm.java
@@ -9,9 +9,11 @@ import java.util.List;
 public class Farm {
 
 	private List<Cow> cows = new ArrayList<Cow>();// 母牛集合
+	private Cow ancestor;// 祖先
 
 	public Farm() {
-		cows.add(new Cow());
+		ancestor = new Cow();
+		cows.add(ancestor);
 	}
 
 	public void grow() {
@@ -41,6 +43,9 @@ public class Farm {
 		}
 
 		System.out.println(farm.count());
+
+		List<Cow> childs = farm.ancestor.getChilds();
+		System.out.println(childs.size());
 	}
 }
 
@@ -50,20 +55,65 @@ public class Farm {
 class Cow {
 
 	private int age;// 年龄
+	private Cow parent;// 父节点
+	private Cow child;// 子节点
+	private Cow sibling;// 兄弟节点
 
 	public Cow() {
 		age = 0;
 	}
 
 	public Cow grow() {
-		Cow child = null;
+		Cow cow = null;
 
 		if (++age >= 4) {
-			child = new Cow();
+			cow = new Cow();
+			cow.parent = this;
+			if (child == null) {// 当前Cow已经存在子节点
+				child = cow;
+			} else {
+				Cow c = child;
+				while (c.sibling != null) {
+					c = c.sibling;
+				}
+				c.sibling = cow;
+			}
 		}
 
-		return child;
+		return cow;
 	}
 
+	/**
+	 * 获取Cow的孩子
+	 * 
+	 * @return
+	 */
+	public List<Cow> getChilds() {
+		List<Cow> result = new ArrayList<Cow>();
+		Cow c = child;
+
+		while (c != null) {
+			result.add(c);
+			c = c.sibling;
+		}
+
+		return result;
+	}
+
+	/**
+	 * 获取Cow的兄弟
+	 * 
+	 * @return
+	 */
+	public List<Cow> getSiblings() {
+		List<Cow> result = null;
+		if (parent != null) {
+			result = parent.getChilds();
+			result.remove(this);
+		} else {
+			result = new ArrayList<Cow>();
+		}
+		return result;
+	}
 }
 /* 农场一小母牛，母牛四岁可生小母牛。问20年后，农场有多少头母牛？ */
