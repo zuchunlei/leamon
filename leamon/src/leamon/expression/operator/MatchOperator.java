04a9c7d1cac5dc4ca7a502579b4e49429fcbc7c7
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-07-01 05:14:39 +0000

    + 添加操作符的实现类CommonOperator与MatchOperator。其中CommonOperator为普通运算符，例如'+'、'-'等还有运算逻辑的运算符。MatchOperator为匹配运算符，主要是作为区段的结束符号，例如')'。

---
+++ leamon/src/leamon/expression/operator/MatchOperator.java
@@ -0,0 +1,50 @@
+package leamon.expression.operator;
+
+import leamon.expression.Operator;
+
+/**
+ * 匹配操作符，主要是类")"
+ */
+public class MatchOperator implements Operator {
+
+	private char op;// 操作符号
+	private int priority;// 优先级
+	private char matcher;// 被匹配操作符
+
+	public MatchOperator(char op, int priority, char matcher) {
+		this.op = op;
+		this.priority = priority;
+		this.matcher = matcher;
+	}
+
+	@Override
+	public double calculate(double leftValue, double rightValue) {
+		return 0;
+	}
+
+	@Override
+	public boolean underPriority(Operator op) {
+		return this.priority <= op.getPriority();
+	}
+
+	@Override
+	public int getPriority() {
+		return this.priority;
+	}
+
+	@Override
+	public boolean isMatch() {
+		return true;
+	}
+
+	@Override
+	public boolean match(char c) {
+		return c == matcher;
+	}
+
+	@Override
+	public char getDescription() {
+		return this.op;
+	}
+
+}
