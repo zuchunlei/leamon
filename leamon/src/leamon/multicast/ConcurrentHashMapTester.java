a34e9454db94a2dc41c891004049fdab6a82f843
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-07-10 09:27:56 +0000

    + 添加ConcurrentHashMap的测试类，该类主要测试ConcurrentHashMap的内部实现原理，以及api的设计思路，从而明确为什么ConcurrentHashMap能够保证在并发下保持数据的正确性。 # ConcurrentHashMap主要的实现原理就是"锁分离"，ConcurrentHashMap内部使用段(Segment)来表示这些不同的部分，每个段其实就是一个独立的hash table，它们有自己的锁。注：Segment继承至ReentrantLock。 # 只要多个修改操作发生在不同的段上，它们就可以并发进行。其中get()操作不加锁，put()操作在段上进行加锁，remove()操作删除指定的条目后，复制原来的链表保证其不变性。 # 其中putIfAbsent()方法相当与if (!map.containsKey(key)){return map.put(key, value);}else{return map.get(key);}。

---
+++ leamon/src/leamon/multicast/ConcurrentHashMapTester.java
@@ -0,0 +1,46 @@
+package leamon.multicast;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 并发Map测试类
+ */
+public class ConcurrentHashMapTester {
+
+	public static void main(String[] args) {
+		/**
+		 * ConcurrentHashMap主要的实现原理就是"锁分离"，ConcurrentHashMap内部使用段(Segment)
+		 * 来表示这些不同的部分，每个段其实就是一个独立的hash table，它们有自己的锁。
+		 * 
+		 * 只要多个修改操作发生在不同的段上，它们就可以并发进行。
+		 */
+		final ConcurrentHashMap<String, Object> map = new ConcurrentHashMap<String, Object>();
+
+		new Thread() {
+			@Override
+			public void run() {
+				map.putIfAbsent("zuchunlei", new Object() {
+					@Override
+					public String toString() {
+						return "zuchunlei";
+					}
+
+				});
+			}
+		}.start();
+
+		new Thread() {
+			@Override
+			public void run() {
+				map.putIfAbsent("zuchunlei", new Object() {
+					@Override
+					public String toString() {
+						return "zuchunlei-1";
+					}
+
+				});
+			}
+		}.start();
+
+	}
+}
