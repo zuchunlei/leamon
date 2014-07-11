a326aafd18500cf03406d147610c50b9a82a0c9f
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-07-11 02:39:50 +0000

    * 变更InsertionSort为SortUtils工具类，该类中主要提供多种排序算法的实现。同步变更sort()为insertionSort()，该方法为插值排序的实现。

--- leamon/src/leamon/datastruct/sort/SortUtils.java
+++ leamon/src/leamon/datastruct/sort/SortUtils.java
@@ -1,9 +1,9 @@
 package leamon.datastruct.sort;
 
 /**
- * 插值排序
+ * 排序工具类，内部提供多种排序算法
  */
-public class InsertionSort {
+public class SortUtils {
 
 	/**
 	 * 插值排序，基于比较交换进行排序。 选择位置p，将array[p]数值与位于p左区域的数据集进行比较。
@@ -11,13 +11,13 @@ public class InsertionSort {
 	 * 
 	 * @param array
 	 */
-	public static void sort(int[] array) {
+	public static void insertionSort(int[] array) {
 
 		for (int p = 1; p < array.length; p++) {
 			int tmp = array[p];// 临时值
 			int j = p;
 			for (; j > 0 && array[j - 1] > tmp; j--) {
-				// 交换操作
+				// 不是交换，而是移动。所有比p位置值大的数据全部向右移动
 				array[j] = array[j - 1];
 			}
 
