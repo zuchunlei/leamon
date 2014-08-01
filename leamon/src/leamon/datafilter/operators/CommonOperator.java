a1c5347c6433ac4f28f11b2c66403f7a7b641448
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-08-01 05:32:38 +0000

    + 添加operators包，该包主要存放操作符实现类的基类，作为操作符注册的基础。 + 同步添加CommonOperator与MatchOperator。其中CommonOperator为普通运算符，例如“&&”、“||”等具有逻辑关系的运算符。MatchOperator为匹配运算符，主要是作为区段的结束符号，例如“)”、“]”等。

---
+++ leamon/src/leamon/datafilter/operators/CommonOperator.java
@@ -0,0 +1,43 @@
+package leamon.datafilter.operators;
+
+import leamon.datafilter.Operator;
+
+public abstract class CommonOperator implements Operator {
+
+	private String desc;// 操作符描述
+	private int priority;// 优先级
+
+	public CommonOperator(String desc, int priority) {
+		this.desc = desc;
+		this.priority = priority;
+	}
+
+	@Override
+	public boolean underPriority(Operator op) {
+		return priority <= op.getPriority();
+	}
+
+	@Override
+	public int getPriority() {
+		return priority;
+	}
+
+	@Override
+	public final boolean isMatch() {// not support
+		return false;
+	}
+
+	@Override
+	public final boolean match(String str) {// not support
+		return false;
+	}
+
+	@Override
+	public String getDescription() {
+		return desc;
+	}
+
+	@Override
+	public abstract boolean doFilter(boolean left, boolean right);
+
+}
