3d26e945b33667866c1ccc35fffbac35513263d5
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-08-01 05:51:40 +0000

    * 添加ParseNode与ParseNodeHeap类型，该类型主要是对字符串进行分割的辅助类型。其中ParseNodeHeap为操作符解析节点的最小堆实现。 * 同步添加extract()与split()方法，通过这两个方法，可以将给定的字符串根据配置的操作符集合进行分割，得到字符串的中序遍历形式的集合。

--- leamon/src/leamon/datafilter/DataFilterFactory.java
+++ leamon/src/leamon/datafilter/DataFilterFactory.java
@@ -1,6 +1,9 @@
 package leamon.datafilter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -32,7 +35,180 @@ public class DataFilterFactory {
 	 * @return
 	 */
 	public static DataFilter buildDataFilter(String express) {
+		ParseNodeHeap heap = new ParseNodeHeap();
+
+		for (String operator : operators.keySet()) {
+			extract(express, operator, 0, heap);
+		}
+
+		List<String> infixList = split(express, heap);
 		return null;
 	}
 
+	/**
+	 * 抽取方法，根据给定字符串，定位相关pattern的位置
+	 * 
+	 * @param express
+	 * @param pattern
+	 * @param start
+	 * @param heap
+	 */
+	private static void extract(String express, String pattern, int start,
+			ParseNodeHeap heap) {
+
+		int length = pattern.length();
+		int index = express.indexOf(pattern, start);
+
+		while (index != -1) {
+			ParseNode node = new ParseNode(index, length);
+			heap.add(node);
+
+			start = index + length;
+			index = express.indexOf(pattern, start);
+		}
+	}
+
+	/**
+	 * 分割方法，将给定的表达式根据操作符进行分割
+	 * 
+	 * @param express
+	 * @param heap
+	 * @return
+	 */
+	private static List<String> split(String express, ParseNodeHeap heap) {
+		List<String> result = new ArrayList<String>();
+
+		LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+		set.add(0);
+		for (int i = 0, size = heap.size(); i < size; i++) {
+			ParseNode node = heap.deleteMin();
+			int index = node.index;
+			int end = index + node.length;
+			set.add(index);
+			set.add(end);
+		}
+		set.add(express.length());
+
+		Object[] array = set.toArray();
+
+		int i = 0;
+		while (i < array.length - 1) {
+			int begin = (Integer) array[i];
+			int end = (Integer) array[i + 1];
+			result.add(express.substring(begin, end));
+			i++;
+		}
+
+		return result;
+	}
+
+	/**
+	 * 字符串解析节点，该节点主要记录操作符所处于给定字符串的位置
+	 */
+	private static class ParseNode implements Comparable<ParseNode> {
+		private int index;
+		private int length;
+
+		public ParseNode(int index, int length) {
+			this.index = index;
+			this.length = length;
+		}
+
+		@Override
+		public int compareTo(ParseNode o) {
+			return index - o.index;
+		}
+	}
+
+	/**
+	 * ParseNode的最小堆实现
+	 */
+	private static class ParseNodeHeap {
+		private static final int DEFAULT_CAPACITY = 10;
+
+		private ParseNode[] array;
+		private int size;
+
+		public ParseNodeHeap() {
+			this(DEFAULT_CAPACITY);
+		}
+
+		public ParseNodeHeap(int capacity) {
+			this.size = 0;
+			this.array = new ParseNode[capacity];
+		}
+
+		private void enlarge(int capacity) {
+			ParseNode[] tmp = new ParseNode[capacity];
+			System.arraycopy(array, 0, tmp, 0, size - 1);
+			array = tmp;
+		}
+
+		public int size() {
+			return size;
+		}
+
+		public boolean isEmpty() {
+			return size == 0;
+		}
+
+		public ParseNode findMin() {
+			if (isEmpty()) {
+				String message = "current heap is empty;";
+				throw new RuntimeException(message);
+			}
+
+			return array[0];
+		}
+
+		public void add(ParseNode node) {
+			if (size == array.length) {
+				enlarge(2 * size);
+			}
+			array[size] = node;
+			percolateUp(size++);
+		}
+
+		private void percolateUp(int hole) {
+			ParseNode node = array[hole];
+			for (; hole > 0 && array[(hole - 1) / 2].compareTo(node) > 0; hole = (hole - 1) / 2) {
+				array[hole] = array[(hole - 1) / 2];
+			}
+			array[hole] = node;
+		}
+
+		public ParseNode deleteMin() {
+			ParseNode node = findMin();
+
+			array[0] = array[--size];
+			percolateDown(0);
+			return node;
+		}
+
+		private void percolateDown(int hole) {
+			ParseNode node = array[hole];
+			int child = -1;
+			for (; leftChild(hole) < size; hole = child) {
+				child = leftChild(hole);
+
+				if (child != size - 1
+						&& array[child + 1].compareTo(array[child]) < 0) {
+					child++;
+				}
+
+				if (array[child].compareTo(node) < 0) {
+					array[hole] = array[child];
+				} else {
+					break;
+				}
+
+			}
+			array[hole] = node;
+
+		}
+
+		private int leftChild(int hole) {
+			return 2 * hole + 1;
+		}
+	}
 }
