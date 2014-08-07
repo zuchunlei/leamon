d9f206bc29f544fcc78cd9816b08a17f22a31ec0
zuchunlei <zuchunlei@263adc20-c66f-fc4e-8458-af51c3f9ae44>
2014-08-07 02:49:28 +0000

    * 添加静态内部类的编译机制，主要关注静态内部类对象访问外围类对象的静态域与实例域之间的区别，以及编译器对外围类access$方法的织入操作。 # 静态内部类的对象不持有对外围类的引用，静态内部类不能直接访问外围的实例域，但是直接可以访问外围类的静态域。 # 当静态内部类访问外围类的静态域或者间接通过外围对象来访问外围类的实例域时，编译器会在外围类中织入静态方法来供静态内部类实例去调用，从而达到访问外围类静态域与实例域的效果！ # 其中访问外围类的静态域，外围类织入的方法没有参数。如果访问外围类的实例域，则织入的方法有参数，且参数为外围类实例对象。当前实例中，编译器为外围类织入的静态方法如下所列： # static Object access$0(OuterClass)对应实例域value，static Date access$1(OuterClass)对应实例域time，static int access$2()对应静态域count，static Object access$3()对应静态域lock。

--- leamon/src/leamon/enhance/OuterClass.java
+++ leamon/src/leamon/enhance/OuterClass.java
@@ -12,16 +12,28 @@ import java.util.Date;
  * access$0(OuterClass)， 其中Object是当前外围类OuterClass实例域value的类型。
  * 
  * 成员内部类访问外围类的多个实例域，编译器就会为外围类声明几个类似access$这样的静态方法！！
+ * 
+ * 当静态内部类访问外围类的静态域或者间接通过外围对象来访问外围类的实例域时，
+ * 编译器会在外围类中织入静态方法来供静态内部类实例去调用，从而达到访问外围类静态域与实例域的效果！
+ * 
  */
 public class OuterClass {
+
+	private static int count = 10;
+
+	private static Object lock = new Object();
 
 	private Object value = new Object();
 
 	private Date time = new Date();
 
-	// static Object access$0(OuterClass p) {return p.value} ; 编译器织入
+	// static Object access$0(OuterClass p) {return p.value ;} 编译器织入
 
-	// static Date access$1(OuterClass p) {return p.time} ; 编译器织入
+	// static Date access$1(OuterClass p) {return p.time ;} 编译器织入
+
+	// static int access$2() {return count ;} 编译器织入
+
+	// static Object access$3() {return lock ;} 编译器织入
 
 	public InnerClass getInnerClassInstance() {
 		return new InnerClass();
@@ -48,6 +60,45 @@ public class OuterClass {
 		public Date getTime() {
 			return time;
 		}
+	}
+
+	/**
+	 * 静态内部类的对象不持有对外围类的引用，静态内部类不能直接访问外围的实例域，但是直接可以访问外围类的静态域。
+	 */
+	static class StaticInnerClass {
+
+		/**
+		 * 当前方法访问外围类的静态域count
+		 */
+		public int getCount() {
+			return count;
+		}
+
+		/**
+		 * 当前方法访问外围类的静态域lock
+		 */
+		public Object getLock() {
+			return lock;
+		}
+
+		/**
+		 * 当前方法访问外围类的实例域value
+		 */
+		public Object getValue() {
+			return getOutClassInstance().value;
+		}
+
+		/**
+		 * 当前方法访问外围类的实例域time
+		 */
+		public Date getTime() {
+			return getOutClassInstance().time;
+		}
+
+		public OuterClass getOutClassInstance() {
+			return new OuterClass();
+		}
+
 	}
 
 }
