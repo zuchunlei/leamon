5832ae76f773991c212b9feaee7236c8012e8b6d
zuchunlei <zuchunlei@outlook.com>
2015-10-09 11:19:51 +0800

    * 格式化java文件，使用4个空格符替换tab。

--- leamon/src/leamon/datafilter/operators/MatchOperator.java
+++ leamon/src/leamon/datafilter/operators/MatchOperator.java
@@ -7,44 +7,44 @@ import leamon.datafilter.Operator;
  */
 public class MatchOperator implements Operator {
 
-	private String desc;// 操作符描述
-	private String matcher;// 被匹配操作符
-	private int priority;// 优先级
+    private String desc;// 操作符描述
+    private String matcher;// 被匹配操作符
+    private int priority;// 优先级
 
-	public MatchOperator(String desc, String matcher, int priority) {
-		this.desc = desc;
-		this.matcher = matcher;
-		this.priority = priority;
-	}
+    public MatchOperator(String desc, String matcher, int priority) {
+        this.desc = desc;
+        this.matcher = matcher;
+        this.priority = priority;
+    }
 
-	@Override
-	public boolean underPriority(Operator op) {
-		return priority <= op.getPriority();
-	}
+    @Override
+    public boolean underPriority(Operator op) {
+        return priority <= op.getPriority();
+    }
 
-	@Override
-	public int getPriority() {
-		return priority;
-	}
+    @Override
+    public int getPriority() {
+        return priority;
+    }
 
-	@Override
-	public final boolean isMatch() {
-		return true;
-	}
+    @Override
+    public final boolean isMatch() {
+        return true;
+    }
 
-	@Override
-	public final boolean match(String str) {
-		return matcher.equals(str);
-	}
+    @Override
+    public final boolean match(String str) {
+        return matcher.equals(str);
+    }
 
-	@Override
-	public String getDescription() {
-		return desc;
-	}
+    @Override
+    public String getDescription() {
+        return desc;
+    }
 
-	@Override
-	public boolean doFilter(boolean left, boolean right) {// not support
-		return false;
-	}
+    @Override
+    public boolean doFilter(boolean left, boolean right) {// not support
+        return false;
+    }
 
 }
