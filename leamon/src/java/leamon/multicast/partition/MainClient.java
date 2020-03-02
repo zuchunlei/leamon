b590c2bd9b14c74100718ecdfd3757cf49054076
zuchunlei <zuchunlei@jd.com>
2020-03-02 09:35:23 +0800

    fix typo

--- leamon/src/java/leamon/multicast/partition/MainClient.java
+++ leamon/src/java/leamon/multicast/partition/MainClient.java
@@ -3,11 +3,11 @@ package leamon.multicast.partition;
 public class MainClient {
 
     public static void main(String[] args) {
-        Downloader downloador = new Downloader();
+        Downloader downloader = new Downloader();
         String src = args[0];
         String dest = args[1];
         long start = System.currentTimeMillis();
-        downloador.download(src, dest);
+        downloader.download(src, dest);
         System.out.println("total cost time is :" + (System.currentTimeMillis() - start));
     }
 }
